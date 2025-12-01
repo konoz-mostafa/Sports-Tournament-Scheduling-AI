183a7dcf43adda5cc207e2c86b54f2862ff4c48b
konoz <konozmostafasedky@gmail.com>
2025-12-02 01:47:48 +0200

    contin

---
+++ data/teams_venues_times.py
@@ -0,0 +1,16 @@
+teams = [
+    "Al Ahly", "Zamalek", "Pyramids", "Masry", "Future", "Ismaily",
+    "Smouha", "ENPPI", "Ceramica", "National Bank", "Talaea El Gaish",
+    "Alexandria Union", "El Dakhleya", "El Gouna", "Zed",
+    "Modern Sport", "Pharco", "Wadi Degla"
+]
+
+venues = [
+    "Cairo Stadium", "Borg El Arab", "Air Defense Stadium",
+    "Suez Stadium", "Alexandria Stadium", "Petro Sport Stadium",
+    "Military Academy Stadium", "Al Salam Stadium", "El Sekka El Hadeed Stadium",
+    "Zed Club Stadium"
+]
+
+match_times = ["17:00", "20:00"]
+
